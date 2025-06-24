a4169eadab1b1eab4c89365193041716396843b0
anatolii-k <anatoly.zp@gmail.com>
2025-06-24 17:54:28 +0300

    Switch to CMSIS

--- 000_Led_Switch_By_Button/Src/main.c
+++ 000_Led_Switch_By_Button/Src/main.c
@@ -8,58 +8,28 @@
  * */
 
 #include <stdint.h>
+#include "stm32f446xx.h"
 
-// REGISTERS
-
-#define RCC_BASE (0x40023800UL)
-#define RCC_AHB1ENR_OFFSET (0x30UL)
-#define RCC_AHB1ENR (*(volatile uint32_t*)( RCC_BASE + RCC_AHB1ENR_OFFSET ))
-#define RCC_GPIOAEN (1 << 0)
-#define RCC_GPIOCEN (1 << 2)
-
-#define GPIO_MODER_OFFSET (0x0)
-#define GPIO_MODER5_MASK (0xC00)
-#define GPIO_MODER13_MASK (0x0C000000UL)
-#define GPIO_MODER5_OUTPUT (1<<10)
-#define GPIO_ODR_OFFSET (0x14)
-#define GPIO_PUPDR_OFFSET (0x0C)
-#define GPIO_PUPDR13_MASK (0xC00)
-#define GPIO_PUPDR13_PULLUP (1<<26)
-#define GPIO_IDR_OFFSET (0x10)
-
-#define GPIOA_BASE (0x40020000UL)
-#define GPIOC_BASE (0x40020800UL)
-#define GPIOA_MODER (*(volatile uint32_t*)(GPIOA_BASE + GPIO_MODER_OFFSET))
-#define GPIOC_MODER (*(volatile uint32_t*)(GPIOC_BASE + GPIO_MODER_OFFSET))
-#define GPIOA_ODR (*(volatile uint32_t*)(GPIOA_BASE + GPIO_ODR_OFFSET))
-#define GPIOC_PUPDR (*(volatile uint32_t*)(GPIOC_BASE + GPIO_PUPDR_OFFSET))
-#define GPIOC_IDR (*(volatile uint32_t*)(GPIOC_BASE + GPIO_IDR_OFFSET))
-
-#define PIN_5 (1 << 5)
-#define PIN_13 (1 << 13)
-
-//////// APPLICATION /////////
-
-#define LED_PIN PIN_5
-#define SWICTH_BUTTON_PIN PIN_13
+#define LED_ODR GPIO_ODR_OD5
+#define SWICTH_BUTTON_IDR GPIO_IDR_ID13
 #define BUTTON_IS_PRESSED (0)
 
 static int lastButtonState = 1;
 
 void setup(){
 	// RCC enable
-	RCC_AHB1ENR |= RCC_GPIOAEN | RCC_GPIOCEN;
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOCEN;
 
 	// output mode for PA5
-	GPIOA_MODER &= ~GPIO_MODER5_MASK;
-	GPIOA_MODER |= GPIO_MODER5_OUTPUT;
+	GPIOA->MODER |=  GPIO_MODER_MODER5_0;
+	GPIOA->MODER &= ~GPIO_MODER_MODER5_1;
 
 	// input mode for PC13
-	GPIOC_MODER &=~GPIO_MODER13_MASK;
+	GPIOC->MODER &= ~GPIO_MODER_MODER13;
 
 	// pull-up for PC13
-	GPIOC_PUPDR &= ~GPIO_PUPDR13_MASK;
-	GPIOC_PUPDR |= GPIO_PUPDR13_PULLUP;
+	GPIOC->PUPDR &= ~GPIO_PUPDR_PUPD13_1;
+	GPIOC->PUPDR |=  GPIO_PUPDR_PUPD13_0;
 }
 
 void makeDelay()
@@ -72,13 +42,13 @@ int main(void)
 	setup();
 	for(;;)
 	{
-		int buttonState = GPIOC_IDR & SWICTH_BUTTON_PIN ? 1 : 0;
+		int buttonState = GPIOC->IDR & SWICTH_BUTTON_IDR ? 1 : 0;
 		if( lastButtonState != buttonState )
 		{
 			lastButtonState = buttonState;
 			if(buttonState == BUTTON_IS_PRESSED)
 			{
-				GPIOA_ODR ^= LED_PIN;
+				GPIOA->ODR ^= LED_ODR;
 			}
 		}
 		makeDelay();
